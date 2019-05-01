b1f3246b81e1084707bf236451a4acb7c78e6db9
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-04-30 23:25:30 -0500

    Fixing PEP8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,5 +11,5 @@ if mod_n > 5:
 elif mod_n == 0:
     print("Last digit of {} is {} and is 0".format(number, mod_n))
 else:
-    print("Last digit of {} is {} and is less than 6 and not 0"\
-        .format(number, mod_n))
+    print("Last digit of {} is {} and is less than 6 and not 0".format(
+        number, mod_n))
