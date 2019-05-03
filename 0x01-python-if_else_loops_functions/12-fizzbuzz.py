1acd337980402d379df725fe3d303cd170002332
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-02 23:21:20 -0500

    Adding code for Task#15

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    values = range(1,101)
+    values = range(1, 101)
     for i in values:
         if ((i % 3 == 0) and (i % 5 == 0)):
             print("FizzBuzz ", end="")
