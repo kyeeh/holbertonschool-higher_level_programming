0f61c936e36b15aab54ade41775cee0fdb35ef18
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-03 19:57:01 -0500

    Adding code for Task#02

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    ac = len(sys.argv) - 1
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(ac))
+    for i in range(1, ac + 1):
+        print("{}: {}".format(i, sys.argv[i]))
