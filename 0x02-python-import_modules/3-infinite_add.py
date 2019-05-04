f5ca15b9932af9a5cca251d0bfaf32d738853cef
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-03 20:04:29 -0500

    Adding code for Task#03

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    sum = 0
+    ac = len(sys.argv) - 1
+    for i in range(1, ac + 1):
+        sum += int(sys.argv[i])
+    print(sum)
