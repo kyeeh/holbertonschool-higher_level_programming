32f1185547a48150dc06d116b1218776aac11249
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-03 22:13:53 -0500

    Adding operator to print

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,17 +3,17 @@ from sys import argv
 from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    if len(argv) != 4:
+    if len(argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    operators = ["+", "-", "*", "/"]
-    functions = [add, sub, mul, div]
+    ops = ["+", "-", "*", "/"]
+    fcs = [add, sub, mul, div]
     for i in range(0, 4):
-        if argv[2] == operators[i]:
+        if argv[2] == ops[i]:
             a = int(argv[1])
             b = int(argv[3])
-            print("{:d} + {:d} = {:d}".format(a, b, functions[i](a, b)))
+            print("{:d} {} {:d} = {:d}".format(a, argv[2], b, fcs[i](a, b)))
             exit(0)
-    print("Unknown operator. Available operators: +, -, * and /")
+    print("Unknown operator. Available ops: +, -, * and /")
     exit(1)
