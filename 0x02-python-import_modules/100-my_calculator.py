93063786ccca5f0a6ecbc2114a520bcdc81a114e
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-03 22:02:59 -0500

    Adding code for Task#06

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,5 +1,20 @@
 #!/usr/bin/python3
-from variable_load_5 import a
+from sys import argv
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    print("{}".format(a))
+    if len(argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operators = ["+", "-", "*", "/"]
+    functions = [add, sub, mul, div]
+    for i in range(0, 4):
+        if argv[2] == operators[i]:
+            a = int(argv[1])
+            b = int(argv[3])
+            print("{} + {} = {}".format(a, b, functions[i](a, b)))
+            exit(0)
+    print("Unknown operator. Available operators: +, -, * and /")
+    exit(1)
+    
