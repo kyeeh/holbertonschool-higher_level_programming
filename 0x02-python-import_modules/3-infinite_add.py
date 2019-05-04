0ab5527dfc2806a183c155ebde8253904a56057e
Ricardo Gutiérrez <ricardo@kyeeh.net>
2019-05-03 20:17:40 -0500

    Adding code for Task#04

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,8 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    sum = 0
     ac = len(sys.argv) - 1
+    if ac == 0:
+        print("0 arguments.")
+    elif ac == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(ac))
     for i in range(1, ac + 1):
-        sum += int(sys.argv[i])
-    print(sum)
+        print("{}: {}".format(i, sys.argv[i]))
