a4ada28d0aa67708d757a04b82a67b9a599b4b2d
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-07 21:35:05 +0000

    Fixing PEP and size validation

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    size = len(matrix)
     for arr in matrix:
+        size = len(arr)
         for i, n in enumerate(arr):
             if (i < size - 1):
-                print("{:d} ".format(n), end = '')
+                print("{:d} ".format(n), end='')
             else:
-                print("{:d}".format(n), end = '')
+                print("{:d}".format(n), end='')
         print("")
