4338da846c251fce2d3d08093dabc649c71f50d4
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-07 21:03:24 +0000

    Fixing str assigments

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    s = (my_string + '.')[:-1]
+    # s = (my_string + '.')[:-1]
+    s = my_string
     s = s.replace("c", "")
     s = s.replace("C", "")
     return (s)
