315bff0df6f1aeed83000b2ac891e7a624da5e0f
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-07 21:31:12 +0000

    Fixing ending spaces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
+    size = len(matrix)
     for arr in matrix:
-        for n in arr:
-            print("{:d} ".format(n), end = '')
+        for i, n in enumerate(arr):
+            if (i < size - 1):
+                print("{:d} ".format(n), end = '')
+            else:
+                print("{:d}".format(n), end = '')
         print("")
