5696339f2d8e28cc96c3ea53ddd27f293c79dc7a
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-09 21:40:00 +0000

    Fixing PEP

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#https://www.programiz.com/python-programming/set
 def only_diff_elements(set_1, set_2):
     return (set_1 ^ set_2)
-#https://www.programiz.com/python-programming/set
+
