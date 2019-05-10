b8b70996ce5833f47889c99ce1a27714b35c1160
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-10 02:04:45 +0000

    Fixing validation

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
     num, den = 0, 0
-    if (len(my_list) > 0):
+    if (my_list and len(my_list) > 0):
         for (score, weight) in my_list:
             num += score * weight
             den += weight
-    return (num / den)
+        num /= den
+    return (num)
