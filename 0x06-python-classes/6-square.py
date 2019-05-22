0a678eaeb83f088a9199919ce6e55361a9d9e226
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-22 03:18:35 +0000

    Fixing validation

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -34,8 +34,8 @@ class Square:
 
     @position.setter
     def position(self, value):
-        if ((type(value) != tuple) or (type(value[0]) != int)
-            or (type(value[1]) != int) or (value[0] < 0)
-                or (value[1] < 0)):
+        if ((type(value) != tuple) or (len(value) != 2)
+            or (type(value[0]) != int) or (type(value[1]) != int)
+                or (value[0] < 0) or (value[1] < 0)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
