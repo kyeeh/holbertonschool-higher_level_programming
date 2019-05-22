e1fd5ac1f5eb3a35223af21b75061cfb5c08f667
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-22 04:59:18 +0000

    Adding code for Node Class

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 class Node:
-    pass
+    def __init__(self, data, next_node=None):
+        self.data = data
+        self.next_node = next_node
+
+    @property
+    def data(self):
+        return (self.__data)
+
+    @data.setter
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError("data must be an integer")
+        self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if (value and type(value) != Node):
+            raise TypeError('next_node must be a Node object')
+        self.__next_node = value
 
 
 class SinglyLinkedList:
-    pass
+    def __init__(self):
+        pass
