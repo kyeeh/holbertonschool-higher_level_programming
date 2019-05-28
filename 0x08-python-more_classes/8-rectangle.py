afe7c1b0cb80df329a10151003ce735dafe69713
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-05-28 04:55:09 +0000

    Fixing static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -3,6 +3,7 @@ class Rectangle:
     number_of_instances = 0
     print_symbol = "#"
 
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if (type(rect_1) != Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
