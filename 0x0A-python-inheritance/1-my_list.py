0105e424de558cf3a4f7c1071d495382e52f385e
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-07 00:43:09 +0000

    Adding test cases

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,4 @@ class MyList(list):
     MyList class that inherits from list
     """
     def print_sorted(self):
-        new_list = self[:]
-        new_list.sort()
-        print(new_list)
+        print(sorted(self))
