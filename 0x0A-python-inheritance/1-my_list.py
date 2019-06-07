8e6f3af477e1a489143c2f9c22e59cc844215f0d
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-07 00:26:31 +0000

    Adding return to sorted list

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,4 +4,6 @@ class MyList(list):
     MyList class that inherits from list
     """
     def print_sorted(self):
-        print(list(sorted(self)))
+        new_list = list(sorted(self))
+        print(new_list)
+        return (new_list)
