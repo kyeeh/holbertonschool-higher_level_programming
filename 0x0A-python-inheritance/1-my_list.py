46843f87bc7cbb6363b1920f2bf41d12b8aa2c73
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-06-07 00:32:38 +0000

    Fixing new list requirement

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,6 +4,6 @@ class MyList(list):
     MyList class that inherits from list
     """
     def print_sorted(self):
-        new_list = list(sorted(self))
+        new_list = self[:]
+        new_list.sort()
         print(new_list)
-        return (new_list)
