9425d52a03c0bfd24783947c60389fab023090cc
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-07-18 04:17:18 +0000

    Fixing errors

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,8 @@
 -- Lists all genres from hbtn_0d_tvshows and displays
 -- the number of shows linked to each.
 
-SELECT tv_show_genres.NAME AS gnre, COUNT(tv_show_genres.NAME) AS number_of_shows
-FROM tv_shows INNER JOIN tv_show_genres
-ON tv_show_genres.genre_id = tv_genres.id
+SELECT tv_genres.name AS genre, COUNT(tv_show_genres.name) AS number_shows
+FROM tv_show_genres INNER JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
 ORDER BY number_shows DESC;
--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,3 +6,4 @@ ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name;
