99731ec47047b46a8f44efdbe2dbf9562f97a0d3
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-08-11 20:41:08 +0000

    Adding case sensitive validation to Task#01

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost",
                          user=argv[1], passwd=argv[2], db=argv[3])
     query = "SELECT * FROM states\
-             WHERE states.name LIKE 'N%'\
+             WHERE states.name LIKE BINARY 'N%'\
              ORDER BY states.id ASC"
     cursor = db.cursor()
     cursor.execute(query)
