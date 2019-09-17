d6413fe1b3775849c25ab3f74d581fa646b50190
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-17 23:58:29 +0000

    Adding code for Tasks#05-07

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // prints the first argument passed to it
 
-if (process.argv.length === 2) {
+if (process.argv.length === undefined) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
