c1af4507ffe4b5a4b95472d8b69c9bf89ade3e6c
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-18 00:01:46 +0000

    Fixing code style

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by 
+// prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
 const langs = ['C is fun', 'Python is cool', 'Javascript is amazing'];
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-// prints 3 lines: (like 1-multi_languages.js) but by 
+// prints 3 lines: (like 1-multi_languages.js) but by
 // using an array of string and a loop
 
 const lang = 'C is fun';
