343eb087d1e4e5b474f599e9a19537ddc96dd9d5
Ricardo Gutierrez <ricardo@kyeeh.net>
2019-09-24 02:42:42 +0000

    Adding base files

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -3,14 +3,23 @@ def pascal_triangle(n):
     """
     returns a list of lists of integers representing the Pascal’s triangle of n
     """
-    pt = [[1], [1, 1]]
-    cl = [1, 1]
-    top = int((n / 2) + 1)
-    for i in range(2, top):
-        for j in range(1, i):
-            #print("{}, {}, {}".format(i, j, top))
-            cl[j] = cl[j] + cl[j - 1]
-        cl.append(1)
-        #print(cl)
-        pt.append(cl)
-    return (pt)
+    if (n <= 0):
+        return []
+    matrix = [[1], [1, 1]]
+    vector = [1, 1]
+    for i in range(3, n):
+        top = int(i / 2)
+        matrix.append(pt_rec(i, top, vector))
+    return (matrix)
+
+def pt_rec(idx, top, vector):
+    """
+    Pascal’s triangle of n in a recursive way
+    """    
+    
+    if (idx >= top):
+        vector[idx - 1] = vector[idx - 1] + vector[idx - 2]
+        return vector
+    else:
+        vector.append(1)
+        pt_rec(idx + 1, top, vector)
